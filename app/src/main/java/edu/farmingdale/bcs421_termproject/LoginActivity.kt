3eceab8736e670f54d92829c3f4d2a40141a66db
jmartinez0 <jmartinez.16c@gmail.com>
2023-11-10 12:40:37 -0500

    Initialized views in login/registration activity controllers

--- app/src/main/java/edu/farmingdale/bcs421_termproject/LoginActivity.kt
+++ app/src/main/java/edu/farmingdale/bcs421_termproject/LoginActivity.kt
@@ -13,6 +13,24 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
+        val loginButton = findViewById<Button>(R.id.logInButton)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        val googleButton = findViewById<Button>(R.id.googleButton)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+
+        loginButton.setOnClickListener {
+            // Code to verify log in information then move to dashboard
+        }
+
+        // Move to registration activity
+        signUpButton.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
+
+        googleButton.setOnClickListener {
+            // Code to continue with a Google account
+        }
     }
 
 }
