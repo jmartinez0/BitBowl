6ab048ccec55399406be6013b7ed7e27404f5081
Jayson Gonzalez <89937039+YsGonzo@users.noreply.github.com>
2023-12-09 06:04:22 -0500

    Sync with Health Connect button now sends user to setting to enable necessary permissions for certain features. Body Measurements fragments now inserts weight records to Health Connect through the api.

--- app/src/main/java/edu/farmingdale/bcs421_termproject/AccountFragment.kt
+++ app/src/main/java/edu/farmingdale/bcs421_termproject/AccountFragment.kt
@@ -1,12 +1,19 @@
 package edu.farmingdale.bcs421_termproject
 
 import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.google.firebase.auth.FirebaseAuth
 import edu.farmingdale.bcs421_termproject.databinding.FragmentAccountBinding
@@ -89,10 +96,28 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
             }
         }
 
+
         syncButton.setOnClickListener {
-            parentFragmentManager.beginTransaction().apply {
-                replace(R.id.frameLayout, syncFragment)
-                commit()
+            val packageName = "edu.farmingdale.bcs421_termproject"
+
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            intent.data = Uri.parse("package:$packageName")
+
+            if (intent.resolveActivity(requireActivity().packageManager) != null) {
+                // Check if the required permissions are granted
+                if (arePermissionsGranted()) {
+                    // Permissions are granted, navigate to SyncFragment
+                    parentFragmentManager.beginTransaction().apply {
+                        replace(R.id.frameLayout, syncFragment)
+                        commit()
+                    }
+                } else {
+                    // Permissions are not granted, open app settings
+                    startActivity(intent)
+                }
+            } else {
+                // Handle the case where there's no activity to handle the intent
+                // You can show a toast or a message to inform the user
             }
         }
 
@@ -103,4 +128,36 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
         return view
     }
 
+    companion object {
+        // Define a constant for your permission request code
+        private const val PERMISSION_REQUEST_CODE = 1001
+    }
+    private fun arePermissionsGranted(): Boolean {
+        val requiredPermissions = arrayOf(
+            "android.permission.health.READ_HEART_RATE",
+            "android.permission.health.WRITE_HEART_RATE",
+            "android.permission.health.READ_EXERCISE",
+            "android.permission.health.WRITE_EXERCISE",
+            "android.permission.health.READ_TOTAL_CALORIES_BURNED",
+            "android.permission.health.WRITE_TOTAL_CALORIES_BURNED",
+            "android.permission.health.READ_WEIGHT",
+            "android.permission.health.WRITE_WEIGHT",
+            "android.permission.health.READ_BASAL_METABOLIC_RATE",
+            "android.permission.health.WRITE_HEIGHT",
+            "android.permission.health.READ_HEIGHT"
+        )
+
+        // Check if all required permissions are granted
+        for (permission in requiredPermissions) {
+            if (ContextCompat.checkSelfPermission(requireContext(), permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Return false if any required permission is not granted
+                return false
+            }
+        }
+
+        // Return true if all required permissions are granted
+        return true
+    }
 }
--- app/src/main/java/edu/farmingdale/bcs421_termproject/BodyMeasurementsFragment.kt
+++ app/src/main/java/edu/farmingdale/bcs421_termproject/BodyMeasurementsFragment.kt
@@ -5,10 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.records.WeightRecord
+import androidx.health.connect.client.units.Mass
+import androidx.lifecycle.lifecycleScope
+import edu.farmingdale.bcs421_termproject.HealthConnectManager
 import edu.farmingdale.bcs421_termproject.databinding.FragmentBodyMeasurementsBinding
 import edu.farmingdale.bcs421_termproject.databinding.FragmentPersonalInformationBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.ZonedDateTime
 
 class BodyMeasurementsFragment : Fragment(R.layout.fragment_body_measurements) {
 
@@ -35,9 +46,38 @@ class BodyMeasurementsFragment : Fragment(R.layout.fragment_body_measurements) {
         val editHeight = view.findViewById<ImageView>(R.id.editHeight)
         val editWeight = view.findViewById<ImageView>(R.id.editWeight)
 
+        val submitButton = view.findViewById<Button>(R.id.submitButton)
+        submitButton.setOnClickListener{
+            val newWeight = weightEditText.text.toString()
+            lifecycleScope.launch(Dispatchers.IO) {
+                writeWeightInput(newWeight.toDouble())
+            }
+        }
         // Click listeners for the image views to make the edit texts editable (they shouldn't be at first).
         // Code to confirm changes and update Firestore.
         return view
+    }
+
+    suspend fun writeWeightInput(weightInput: Double) {
+        val time = ZonedDateTime.now().withNano(0)
+        val weightRecord = WeightRecord(
+            weight = Mass.pounds(weightInput),
+            time = time.toInstant(),
+            zoneOffset = time.offset
+        )
+        val records = listOf(weightRecord)
+        try {
+            val healthConnectClient by lazy { context?.let { HealthConnectClient.getOrCreate(it) } }
+            healthConnectClient?.insertRecords(records)
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, "Successfully insert records", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, e.message.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
